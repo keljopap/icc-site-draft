ff600d5aa59ebe3edf8b6434432c07a6ce4f3fcf
Kelli <keljopap@umich.edu>
2016-04-04 13:46:20 -0400

    first commit

---
+++ icc.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head><title>My First PHP Script</title></head>
+<body>
+	<h1> my first PHP script </h1>
+	<p> Welcome, Internet user from IP address <?echo $REMOTE_ADDR?>.  Hope you like my first PHP page.  </p>
+
+</body>
+
+</html>
